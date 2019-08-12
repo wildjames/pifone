57f2c7398f7f98995d48bd80dd8e8bcacde0bd6c
wildjames <jwild2@sheffield.ac.uk>
2019-08-12 19:17:13 +0100

    Hopefully helpful logging

--- pifone.py
+++ pifone.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
+import pyaudio
+
 from audio_handler import Listener
 
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+
 lis = Listener()
-lis.CHUNK = 1024
-lis.RATE  = 48000
