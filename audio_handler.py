8e6a8325f6aaec75882b321475ff3d35d957181c
wildjames <jwild2@sheffield.ac.uk>
2019-09-03 20:32:26 +0100

    Cant find files

--- audio_handler.py
+++ audio_handler.py
@@ -259,6 +259,7 @@ class Listener():
 
     def play_cummy(self):
         '''play a cum file'''
+        print(os.listdir('AUDIO_FILES'))
         fnames = Path('.').glob("**/CUM/*.wav")
         try:
             playme = random.choice(fnames)
