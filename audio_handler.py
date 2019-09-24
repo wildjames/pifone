2d0b30b789fac9cbc3eaee3f4e7e87fe26a310f2
wildjames <jwild2@sheffield.ac.uk>
2019-09-25 00:36:51 +0100

    Interrupting should stop, not pause

--- audio_handler.py
+++ audio_handler.py
@@ -503,10 +503,10 @@ class Listener():
 
         while p.get_state() == vlc.State.Playing:
             if self._interrupt:
-                p.pause()
+                p.stop_async()
                 print("is p playing? {}".format(p.get_state()))
             if not self._handset_is_up:
-                p.pause()
+                p.stop_async()
                 print("is p playing? {}".format(p.get_state()))
 
         self.dialtone('tone')
