3921689c43e1901c7eb6e27b15ac5cb331e842ae
wildjames <jwild2@sheffield.ac.uk>
2019-09-28 09:33:48 +0100

    fsd

--- audio_handler.py
+++ audio_handler.py
@@ -150,7 +150,7 @@ class Listener():
             stream = self.player.open(
                 output_device_index=self.DEVICE_INDEX,
                 format=fmt,
-                channels=audio_file.getnchannels(),
+                channels=self.CHANNELS,
                 rate=audio_file.getframerate(),
                 output=True,
                 frames_per_buffer=self.CHUNK
@@ -287,18 +287,25 @@ class Listener():
         samples *= volume
 
         samples = samples.astype(np.float32)
-        print(samples)
+
+        volume = 0.5     # range [0.0, 1.0]
+        fs = 44100       # sampling rate, Hz, must be integer
+        duration = 1.0   # in seconds, may be float
+        f = 440.0        # sine frequency, Hz, may be float
+
+        # generate samples, note conversion to float32 array
+        samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
 
         # for paFloat32 sample values must be in range [-1.0, 1.0]
         stream = self.player.open(
-            output_device_index=self.DEVICE_INDEX,
             format=pyaudio.paFloat32,
-            channels=self.CHANNELS,
-            rate=self.RATE,
+            channels=1,
+            rate=fs,
             output=True
         )
 
-        stream.write(samples)
+        # play. May repeat with different volume values (if done interactively)
+        stream.write(volume*samples)
 
         stream.stop_stream()
         stream.close()
