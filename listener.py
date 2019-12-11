0960a605088a3948eed05de90e493a0951f31507
wildjames <jwild2@sheffield.ac.uk>
2019-12-11 22:29:44 +0000

    Recording works. Playback works. Tests pass fine

--- listener.py
+++ listener.py
@@ -5,6 +5,13 @@ from pathlib import Path
 from random import choice
 
 import multiprocessing
+import threading
+
+# Only needed for debugging
+import time
+from pprint import pprint
+
+
 
 class Dictaphone(object):
     '''
@@ -22,19 +29,33 @@ class Dictaphone(object):
       - chunk_size, int:
         - The number of bytes of each playback chunk
     '''
+    RATE = 44100
     CHUNKSIZE = 1024
+    N_CHANNELS = 1
+    FORMAT = pyaudio.paInt16
+
     _stop_playback = False
+    _stop_recording = False
+
     LOUD = 4
 
-    def __init__(self, chunk_size=None, audio_dir='.', **kwargs):
+    def __init__(self, audio_dir='.', rate=None, rec_format=None, chunk_size=None, n_channels=None, **kwargs):
         '''Set up the dictaphone's audio stream'''
 
+        if rate is not None:
+            self.RATE = rate
         if chunk_size is not None:
             self.CHUNKSIZE = chunk_size
+        if n_channels is not None:
+            self.N_CHANNELS = n_channels
+        if rec_format is not None:
+            self.FORMAT = rec_format
 
         self.audio_dir = audio_dir
 
-        self.audio_thread = multiprocessing.Process()
+        # Create blank versions of objects I'll refer to later
+        # self.thread = multiprocessing.Process()
+        self.thread = threading.Thread()
 
         return
 
@@ -45,7 +66,7 @@ class Dictaphone(object):
         Raises an error if it doesn't exist.
         '''
 
-        if self.audio_thread.is_alive():
+        if self.thread.is_alive():
             print("Already playing!")
             return
 
@@ -53,11 +74,12 @@ class Dictaphone(object):
         args = args
         kwargs = kwargs
 
-        self.audio_thread = multiprocessing.Process(
+        # self.thread = multiprocessing.Process(
+        self.thread = threading.Thread(
             target=target, args=args, kwargs=kwargs,
             daemon=True,
         )
-        self.audio_thread.start()
+        self.thread.start()
 
     def play_random(self):
         '''Play a random audio file from my audio_files directory.
@@ -78,6 +100,76 @@ class Dictaphone(object):
 
         self.play_file(fname)
 
+    def make_recording(self, fname=''):
+        '''
+        Create a recording in the directory: <self.audio_dir>/RECORDED
+        with the filename specified. If none is given, the
+        '''
+        rec_dir = os.path.join(self.audio_dir, 'RECORDED')
+
+        if fname == '':
+            # get the highest filename number in the recording dir
+            max_num = 0
+            for filename in Path(rec_dir).rglob('*.wav'):
+                filename = os.path.split(filename)[-1]
+                file_num = os.path.splitext(filename)[0]
+
+                if file_num.isdigit():
+                    file_num = int(file_num)
+                    if file_num >= max_num:
+                        max_num = file_num + 1
+
+            oname = "{:>04d}.wav".format(max_num)
+            oname = os.path.join(rec_dir, oname)
+
+        else:
+            oname = os.path.join(rec_dir, fname)
+        if os.path.isfile(oname):
+            print("File '{}' already exists! Not recording over it.".format(oname))
+            return
+
+        # I'll overwrite the process termination to wait for this flag to become True,
+        # so that my file writing always works properly
+        self._stop_recording = False
+
+        if self.LOUD > 0:
+            print("Making a recording, saving to {}".format(oname))
+
+        # Open an audio stream for recording
+        audio_stream = pyaudio.PyAudio()
+
+        rec_stream = audio_stream.open(
+            format=self.FORMAT,
+            channels=self.N_CHANNELS,
+            rate=self.RATE,
+            frames_per_buffer=self.CHUNKSIZE,
+            input=True
+        )
+
+        # Write the data to a file
+        audio_file = wave.open(oname, 'wb')
+        audio_file.setnchannels(self.N_CHANNELS)
+        audio_file.setsampwidth(audio_stream.get_sample_size(self.FORMAT))
+        audio_file.setframerate(self.RATE)
+
+        # TODO:  (SAFE ON RPI???)
+        # Record all the frames we want, writing them as we go
+        while not self._stop_recording:
+            try:
+                frame = rec_stream.read(self.CHUNKSIZE)
+                audio_file.writeframes(frame)
+            except:
+                break
+
+        # Close the stream
+        rec_stream.stop_stream()
+        rec_stream.close()
+        audio_stream.terminate()
+        audio_file.close()
+
+        # Done recording, raise flag
+        self._stop_recording = False
+
     def play_file(self, fname):
         '''Play the audio file, fname.
         If something is already playing, stop it.
@@ -87,8 +179,11 @@ class Dictaphone(object):
           - fname, str:
             - The file to be played
         '''
-        self.audio_stream = pyaudio.PyAudio()
 
+        # Open audio stream for reading
+        audio_stream = pyaudio.PyAudio()
+
+        # Check the audio file exists. If it does, open it for reading
         exists = os.path.isfile(fname)
         if self.LOUD > 2:
             print("fname:\n'{}'".format(fname))
@@ -97,34 +192,41 @@ class Dictaphone(object):
             return
         audio_file = wave.open(fname, 'rb')
 
-        # Get the format of the audio file
+        # Get the format of the audio file, number of channels (L,R speakers?), and framerate
         width = audio_file.getsampwidth()
-        fmt = self.audio_stream.get_format_from_width(width)
+        fmt = audio_stream.get_format_from_width(width)
         n_channels = audio_file.getnchannels()
         rate = audio_file.getframerate()
 
-        stream = self.audio_stream.open(
+        # Begin stream. This gets written to like a sdtout, only it comes
+        # out of your speakers not your terminal
+        stream = audio_stream.open(
             format=fmt,
             channels=n_channels,
             rate=rate,
             output=True,
         )
 
+        # Loop through, reading the data and playing it.
         data = audio_file.readframes(self.CHUNKSIZE)
         while data != '' and not self._stop_playback:
             stream.write(data)
             data = audio_file.readframes(self.CHUNKSIZE)
 
+        # close stuff gracefully.
         stream.stop_stream()
         stream.close()
+        audio_stream.terminate()
 
-        self.audio_stream.terminate()
-
-        return
+        self._stop_playback = False
 
     def interrupt_playback(self):
-        self.audio_thread.terminate()
+        # self.thread.terminate()
+        self._stop_playback = True
 
+    def stop_recording(self):
+        self._stop_recording = True
+        # self.thread.terminate()
 
 
 class PhoneMonitor(object):
--- tests.py
+++ tests.py
@@ -1,37 +1,54 @@
 import time
 from listener import Dictaphone
+import os
 
+
+###### TEST THE DICTAPHONE OBJECT ######
+test_playback = True
+test_recording = True
 
 
 # Play a test file with the dictaphone
 fname = "AUDIO_FILES/mortal_kombat.wav"
-dic = Dictaphone(audio_dir='AUDIO_FILES/CUM')
+dic = Dictaphone(audio_dir='AUDIO_FILES')
 
 
-print("Playing a random file...")
-dic.start("play_random")
-time.sleep(5)
+#### Playback ####
+if test_playback:
+    print("Playing a random file...")
+    dic.start("play_random")
+    time.sleep(5)
 
-# Interrupt playback
-dic.interrupt_playback()
-print("Interrupt sent")
-time.sleep(5)
+    # Interrupt playback
+    dic.interrupt_playback()
+    print("Interrupt sent - Music should STOP")
+    time.sleep(5)
 
-print("Restarting playback...")
-dic.start("play_file", fname)
-time.sleep(5)
+    print("Restarting playback - Music should BEGIN")
+    dic.start("play_file", fname)
+    time.sleep(5)
 
-print("Trying to start playback over previous playback...")
-dic.start('play_file', fname)
-time.sleep(10)
+    print("Trying to start playback over previous playback - Music should CONTINUE, NOTHING should begin")
+    dic.start('play_file', fname)
+    time.sleep(10)
 
-dic.interrupt_playback()
-print("Termination sent!")
-time.sleep(5)
-
-
+    dic.interrupt_playback()
+    print("Termination sent! Music should STOP")
+    time.sleep(5)
 
 
+#### Recording ####
+if test_recording:
+    print("Making a recording in the RECORDED dir, called TEST.wav, 10s long")
+    if os.path.isfile("AUDIO_FILES/RECORDED/TEST.wav"):
+        os.remove("AUDIO_FILES/RECORDED/TEST.wav")
+
+    dic.start('make_recording', 'TEST.wav')
+    print("Started!")
+
+    time.sleep(10)
+    dic.stop_recording()
+    print("Done!")
 
 print("Done testing.")
 
