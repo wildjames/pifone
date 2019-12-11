8078e815056a8b8de3283ad89f32b82bfba2b963
wildjames <jwild2@sheffield.ac.uk>
2019-12-11 13:04:00 +0000

    Added skeleton docstrings for first few classes

---
+++ listener.py
@@ -0,0 +1,42 @@
+class Dictaphone(object):
+    '''
+    This class acts as a dictaphone (obviously). It should have the following capabilities:
+      - play a recorded message
+      - record a new message
+
+    How hard can that be?
+    '''
+
+
+
+class PhoneMonitor(object):
+    '''
+    This class should listen for two things:
+      - Buttons being pressed currently
+      - If the handset is lifted or not
+
+    It should also track a few things:
+      - Has the button's corresponding function been called already
+      - What button sequence has been pushed
+
+    The button sequence should be cleared when the handset is set down.
+    '''
+
+
+
+class Phone(object):
+    '''
+    This class holds all the bits for the phone, and commands them all.
+
+    It should be able to do the following:
+      - When the handset is lifted, start the intro clip playing
+      - When a button is pushed, play the corresponding DTMF tone
+      - When the user presses the correct button, play a random old recording
+      - When the user presses the correct button, start making a new recording
+      - When the user presses the correct button, play the most recent recording
+      - When the handset is replaced, stop all playback and clear the button sequence
+
+    This should all be able to handle stuff happening asyncronously - the user
+    can press a button during playback, and a tone should still play OVER the
+    existing stream.
+    '''
