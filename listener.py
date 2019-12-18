adc685c774dcccff0962f07b80b08238ed236840
wildjames <jwild2@sheffield.ac.uk>
2019-12-18 22:10:08 +0000

    Dont define a chunksize when playing a dialtone, it breaks on the pi

--- listener.py
+++ listener.py
@@ -127,7 +127,6 @@ class Dictaphone(object):
             channels=self.N_CHANNELS,
             rate=self.RATE,
             output=True,
-            frames_per_buffer=self.CHUNKSIZE,
             output_device_index=self.DEVICE_INDEX,
         )
         stream.write(samples)
