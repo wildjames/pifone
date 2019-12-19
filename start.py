85a557341b80acc9f395e6553d306a768134b480
wildjames <jwild2@sheffield.ac.uk>
2019-12-19 19:02:55 +0000

    Making it so the script can kill its watcher

--- start.py
+++ start.py
@@ -4,5 +4,8 @@ from subprocess import Popen
 filename = 'pifone.py'
 while True:
     print("\nStarting " + filename)
-    p = Popen("python3 " + filename, shell=True)
-    p.wait()
+    try:
+        p = Popen("python3 " + filename, shell=True)
+        p.wait()
+    except SystemExit:
+        break
