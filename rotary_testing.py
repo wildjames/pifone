2454255f061a6ec9b9983299bbc35b107e0543fc
wildjames <jwild2@sheffield.ac.uk>
2020-01-05 17:52:48 +0000

    Trigger testing

--- rotary_testing.py
+++ rotary_testing.py
@@ -8,7 +8,7 @@ counter_pin = 3
 def trigger_activated():
     print("Trigger activated!")
 
-trigger_button = gpiozero.Button(trigger_pin)
+trigger_button = gpiozero.Button(trigger_pin, pull_up=False, active_state=False)
 
 print("OK, go!")
 
